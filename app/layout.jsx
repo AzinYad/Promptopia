b8458fde9e7a2526c65a2711aa78ca5725cf613e
AzinYad <azin.yadegari@gmail.com>
2023-06-07 15:23:13 +0100

    initial setup

---
+++ app/layout.jsx
@@ -0,0 +1,23 @@
+import "@styles/globals.css" 
+
+export const metadata={
+    title: "promptopia",
+    description: "Discover & Share AI Prompts"
+}
+
+const RootLayout = ({Children}) => {
+  return (
+    <html lang= "en">
+        <body>
+            <div className="main">
+                <div className="gradient" />
+            </div>
+            <main className="app">
+                {Children}
+            </main>
+        </body>
+    </html>
+  )
+}
+
+export default RootLayout
---
+++ app/page.jsx
@@ -0,0 +1,9 @@
+
+
+const Home = () => {
+  return (
+    <div>Home hi</div>
+  )
+}
+
+export default Home
