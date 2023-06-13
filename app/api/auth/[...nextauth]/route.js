9251352e3e954d24b2080ed3defee54e59f024b3
AzinYad <azin.yadegari@gmail.com>
2023-06-13 20:10:45 +0100

    add sign in

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
+import User from '@models/user';
 import { connectToDB } from "@utils/database";
 
 const handler = NextAuth({
@@ -17,8 +18,18 @@ const handler = NextAuth({
             // serverless -> lambda -> dynamodb
             await connectToDB();
             // check if the user already exists
+            const userExists = await User.findOne({
+                email: profile.email
+            })
 
             // if not, create a new user
+            if (!userExists) {
+                await User.create({
+                    email: profile.email,
+                    username: profile.name.replace(" ", "").toLowerCase(),
+                    image: profile.picture
+                })
+            }
             return true;
         } catch (error) {
             console.log(error)
